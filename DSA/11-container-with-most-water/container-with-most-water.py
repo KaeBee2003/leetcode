3b3bb3d6d1f83a5564479a4ace9eb22b173ceabd
Karthik Balaji <95918781+KaeBee2003@users.noreply.github.com>
2026-07-17 21:21:55 +0530

    Time: 63 ms (26.62%) | Memory: 29.5 MB (67.68%) - LeetSync

--- DSA/11-container-with-most-water/container-with-most-water.py
+++ DSA/11-container-with-most-water/container-with-most-water.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         i,j = 0,len(height)-1
-        v = min(height[i], height[j])*(j-i)
+        max_vol = min(height[i], height[j])*(j-i)
         
         while i<j:
-            
+
             if min(height[i], height[j]) == height[i]:
                 i += 1
             else:
                 j -= 1
             
-            if min(height[i], height[j])*(j-i) > v:
-                v = min(height[i], height[j])*(j-i)
+            curr_vol = min(height[i], height[j])*(j-i)
+            if curr_vol > max_vol:
+                max_vol = curr_vol
 
-        return v
+        return max_vol
